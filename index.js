63448635f99b97f8bcf10371732b4258607ffd18
vzapata18 <valeriezapata117@gmail.com>
2024-12-01 21:02:52 -0800

    search functionality using api

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+function refreshWeather(response) {
+  console.log(response.data);
+  let tempElement = document.querySelector("#current-temp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "09e7cf3bbc5aa1ao49a9ct04cebc4d31";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Portland");
